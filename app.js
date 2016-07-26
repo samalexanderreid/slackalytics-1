7763533760e27d7717faeb6a75c3d4f291e0d015
samalexanderreid <samalexanderreid@gmail.com>
2016-07-26 18:49:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post('/collect', function(req, res){
 	//Structure Data
 	var data = {
 		v: 		1,
-		tid: 	env_var.ga_key,
+		tid: 	"UA-81348031-2",
 		cid: 	user.id,
 		ds:  	"slack", //data source
 		cs: 	"slack", // campaign source
